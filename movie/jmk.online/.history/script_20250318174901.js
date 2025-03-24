7ff3c1dc901ca11572fdb4d211cbf23ab09dfdce
gama113 <prakash.kumar.sah.2015@gmail.com>
2025-03-24 18:36:28 +0530

    all the updated files is being uploaded

---
+++ movie/jmk.online/.history/script_20250318174901.js
@@ -0,0 +1,50 @@
+///////////////////////////////////////////////////////////
+// smooth scrlling animation
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+////////////////////////////////////////////////////////////
+// for side pannel
+function toggleSidebar() {
+  var sidebar = document.getElementById("mySidebar_sidepanel");
+  if (sidebar.style.width === "300px") {
+    sidebar.style.width = "0";
+  } else {
+    sidebar.style.width = "300px";
+  }
+}
+
+////////////////////////////////////////////////////////////
+// for mail to function
+function validateForm() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  if (name == "" || email == "" || message == "") {
+    alert("All fields must be filled out");
+    return false;
+  }
+
+  var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  if (!emailPattern.test(email)) {
+    alert("Please enter a valid email address");
+    return false;
+  }
+
+  var mailto_link =
+    "mailto:info@jmkonline.co.in?subject=Message from " +
+    name +
+    "&body=" +
+    encodeURIComponent(message + "\n\nFrom: " + name + "\nEmail: " + email);
+  window.location.href = mailto_link;
+  return false;
+}
